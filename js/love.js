49b9e4bb372964da6690b2f977b051179a0150f4
Mazharul islam <miforbd@gmail.com>
2022-03-12 16:49:28 +0600

    inside page design

---
+++ js/love.js
@@ -0,0 +1,6 @@
+document.getElementById('agree').addEventListener('click',()=>{
+    window.location.href="inside.html"
+})
+document.getElementById('sure').addEventListener('click',()=>{
+    window.location.href="inside.html"
+})
