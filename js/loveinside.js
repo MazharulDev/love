f2af8fe2c24b069f50bcfd4731900516e1f94d51
Mazharul Islam <mazharulislam.dev@gmail.com>
2022-11-04 22:18:49 +0600

    name click show pic code comment

--- js/loveinside.js
+++ js/loveinside.js
@@ -1,6 +1,6 @@
-document.getElementById('her-name').addEventListener('click', () => {
-    window.location.href = "misty.html"
-})
+// document.getElementById('her-name').addEventListener('click', () => {
+//     window.location.href = "misty.html"
+// })
 document.getElementById('my-name').addEventListener('click', () => {
     window.location.href = "mazharul.html"
 })
